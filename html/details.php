60eba15532a9bd0a408f7109aafafd2af0bbe510
Amandeep <amanr0878@gmail.com>
2018-02-14 09:36:53 +0530

    init

---
+++ html/details.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Details</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div class="section">
+			<div class="logo">
+			<a href="home.html"><img src="images/logo1.jpg" id="logoImage" ></a>
+			</div>
+			<ul>
+				<li>
+					<a href="home.html">home</a>
+				</li>
+				<li>
+					<a href="about.html">about</a>
+				</li>
+				<li class="selected">
+					<a href="products.html">products</a>
+				</li>
+				<li>
+					<a href="news.html">news</a>
+				</li>
+				<li>
+					<a href="contact.html">contact</a>
+				</li>
+                <li>
+					<a href="login.html">login</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="body">
+		<h3>Products</h3>
+		<ul>
+			
+			<li>
+				<a href="#"><img src="images/img17.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href="#"><img src="images/img18.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href="#"><img src="images/img19.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href="#"><img src="images/img20.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href="#"><img src="images/img21.jpg" alt=""></a>
+			</li>
+		
+		</ul>
+		<a href="#" class="more">show more</a>
+	</div>
+	<div id="footer">
+		<div>
+			<div class="connect">
+                <a href="https://twitter.com/" id="twitter">twitter</a>
+                <a href="http://facebook.com" id="facebook">facebook</a>
+                <a href="http://googleplus.com" id="googleplus">googleplus</a>
+                
+			</div>
+			<p>
+            &copy; copyright 2017 | created by Amandeep kaur, Manpreet sran and Chirag Hans.
+			</p>
+		</div>
+	</div>
+</body>
+</html>
